24a466ecff946af304bec225927c3fe7ef42818a
Kiptev Nikita <n.kiptev@visionlabs.ai>
2024-06-27 23:27:15 +0700

    Add opengraph

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
 export default function CustomersPage() {
   return (
     <p>Customers Page</p>
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,16 +1,29 @@
+'use client';
+ 
 import { lusitana } from '@/app/ui/fonts/fonts';
-import { AtSymbolIcon, KeyIcon, ExclamationCircleIcon } from '@heroicons/react/24/outline';
+import {
+  AtSymbolIcon,
+  KeyIcon,
+  ExclamationCircleIcon,
+} from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from './button';
-
+import { Button } from '@/app/ui/button';
+import { authenticate } from '@/app/lib/actions';
+ 
 export default function LoginForm() {
+
   return (
-    <form className="space-y-3">
+    <form action={authenticate} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`${lusitana.className} mb-3 text-2xl`}>Please log in to continue.</h1>
+        <h1 className={`${lusitana.className} mb-3 text-2xl`}>
+          Please log in to continue.
+        </h1>
         <div className="w-full">
           <div>
-            <label className="mb-3 mt-5 block text-xs font-medium text-gray-900" htmlFor="email">
+            <label
+              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              htmlFor="email"
+            >
               Email
             </label>
             <div className="relative">
@@ -26,7 +39,10 @@ export default function LoginForm() {
             </div>
           </div>
           <div className="mt-4">
-            <label className="mb-3 mt-5 block text-xs font-medium text-gray-900" htmlFor="password">
+            <label
+              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              htmlFor="password"
+            >
               Password
             </label>
             <div className="relative">
@@ -46,7 +62,12 @@ export default function LoginForm() {
         <Button className="mt-4 w-full">
           Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
-        <div className="flex h-8 items-end space-x-1">{/* Add form errors here */}</div>
+        <div
+          className="flex h-8 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
+        >
+        </div>
       </div>
     </form>
   );

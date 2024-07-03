55125e376a6c365d51774da2b4eadce2d7ef5989
Kiptev Nikita <n.kiptev@visionlabs.ai>
2024-07-03 13:04:22 +0700

    Fix build errors

--- app/ui/widgets/PageError/ui/PageError.tsx
+++ app/ui/widgets/PageError/ui/PageError.tsx
@@ -1,29 +1,23 @@
 import React from 'react';
 import cls from './PageError.module.scss';
-import classNames from 'shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
-import { MyButton } from 'shared/ui/Button';
-import { LanguageSwitcher } from 'widget/LanguageSwitcher';
+import classNames from 'clsx';
+import { Button } from '@/app/ui/shared/Button/index';
 
 export interface PageErrorProps {
   readonly className?: string;
 }
 
 function PageError({ className }: PageErrorProps) {
-  const { t } = useTranslation();
   return (
     <div className={classNames(cls.pageError, {}, [className])}>
-      {t('Что-то пошло не так')}
 
-      <MyButton
+      <Button
         onClick={() => {
           location.reload();
         }}
       >
-        {t('Обновить страницу')}
-      </MyButton>
+      </Button>
 
-      <LanguageSwitcher />
     </div>
   );
 }

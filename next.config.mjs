f9b7f0df3de5b97568d407a5b02f7986d1a5062f
Kiptev Nikita <kiptevjs@gmail.com>
2024-07-22 11:42:58 +0700

    Add next config for images

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'api.thaisell.net',
+        port: '',
+        pathname:'/**',
+      }
     ],
   },
   sassOptions: {

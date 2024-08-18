1a6a1c7953112b4d2ad0e5aeb0a53929b43c7e0e
Kiptev Nikita <kiptevjs@gmail.com>
2024-08-19 00:06:12 +0700

    Comment new AdModel

--- app/src/models/supabase/adModel.ts
+++ app/src/models/supabase/adModel.ts
@@ -1,54 +1,54 @@
-import { IDBResponse } from '../../types';
-import { Ad } from '@/app/ui/entities/Ad/types/index';
-import { supabase } from './database';
+// import { IDBResponse } from '../../types';
+// import { Ad } from '@/app/ui/entities/Ad/types/index';
+// import { supabase } from './database';
 
-class AdModel {
-  async findOneById(id:number):Promise<IDBResponse<Ad>>{
-    const response = await supabase
-      .from('ads')
-      .select().eq('id', id);
-    return {data:response.data};
-  }
+// class AdModel {
+//   async findOneById(id:number):Promise<IDBResponse<Ad>>{
+//     const response = await supabase
+//       .from('ads')
+//       .select().eq('id', id);
+//     return {data:response.data};
+//   }
 
-  async findMany(): Promise<IDBResponse<Ad[]>> {
-    const response = await supabase
-      .from('ads')
-      .select();
-    return {data:response.data};
-  }
+//   async findMany(): Promise<IDBResponse<Ad[]>> {
+//     const response = await supabase
+//       .from('ads')
+//       .select();
+//     return {data:response.data};
+//   }
 
-  async create(ad:Ad):Promise<IDBResponse<Ad>>{
-    const imagesArray = `{${ad?.images?.map(image => `"${image}"`).join(',')}}`;
-    const contactsJson = JSON.stringify(ad.contacts);
+//   async create(ad:Ad):Promise<IDBResponse<Ad>>{
+//     const imagesArray = `{${ad?.images?.map(image => `"${image}"`).join(',')}}`;
+//     const contactsJson = JSON.stringify(ad.contacts);
 
-    const response = await supabase
-      .from('ads')
-      .insert({ title:ad.title, price:ad.price, location:ad.location, description:ad.description, images:imagesArray, userid:ad.userid, currency:ad.currency, contacts:contactsJson })
-      .select();
-    return response;
-  }
+//     const response = await supabase
+//       .from('ads')
+//       .insert({ title:ad.title, price:ad.price, location:ad.location, description:ad.description, images:imagesArray, userid:ad.userid, currency:ad.currency, contacts:contactsJson })
+//       .select();
+//     return response;
+//   }
 
-  async search(keyword: string): Promise<IDBResponse<Ad[]>> {
-    const query = `
-        SELECT *
-        FROM ads
-        WHERE similarity(title, '${keyword}') > 0.1
-           OR similarity(description, '${keyword}') > 0.1
-        ORDER BY GREATEST(similarity(title, '${keyword}'), similarity(description, '${keyword}')) DESC;
-    `;
+//   async search(keyword: string): Promise<IDBResponse<Ad[]>> {
+//     const query = `
+//         SELECT *
+//         FROM ads
+//         WHERE similarity(title, '${keyword}') > 0.1
+//            OR similarity(description, '${keyword}') > 0.1
+//         ORDER BY GREATEST(similarity(title, '${keyword}'), similarity(description, '${keyword}')) DESC;
+//     `;
 
-    const { data, error } = await supabase
-      .rpc('execute_sql', { query });
+//     const { data, error } = await supabase
+//       .rpc('execute_sql', { query });
 
-    if (error) {
-      console.error('Error:', error);
-      return { data: [], error: error.message };
-    }
+//     if (error) {
+//       console.error('Error:', error);
+//       return { data: [], error: error.message };
+//     }
 
-    return { data };
-  }
-}
+//     return { data };
+//   }
+// }
 
-const model = new AdModel();
+// const model = new AdModel();
 
-export default model;
+// export default model;

2386a35410fecbdfefc5a4a36c718d0912bea707
Kiptev Nikita <kiptevjs@gmail.com>
2024-09-21 15:46:25 +0700

    Redirect unauthorized users from myAds page

--- app/myAds/page.tsx
+++ app/myAds/page.tsx
@@ -1,29 +1,36 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import styles from './ProfilePage.module.scss';
 import api from '../src/http';
 import MyAdsAdCard from '../ui/entities/Ad/ui/MyAdsAdCard';
 import { Ad } from '../ui/entities/Ad/types';
 import { Loader } from '../ui/shared/Loader';
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
+import { AxiosError } from 'axios';
 
 const MyAdsPage = () => {
   const [myAds, setMyAds] = useState<Ad[]>([]);
   const [isLoading, setIsLoading] = useState(true);
-  const fetchMyAds = async ()=>{
+  const router = useRouter();
+  const fetchMyAds =useCallback( async ()=>{
     try {
       setIsLoading(true);
       const response = await api.get('/ads/myAds');
       setMyAds( response?.data );
     } catch (error) {
-      console.log(error);
+      if (error instanceof AxiosError && error.response?.status === 401) {
+        toast.error('Сначала нужно авторизоваться', { position: 'bottom-left' });
+        router.push('/login');
+      }
     } finally {
       setIsLoading(false);
     }
-  };
+  },[router]);
 
   useEffect(() => {
     fetchMyAds();
-  }, []);
+  }, [fetchMyAds]);
 
   if(isLoading) return <Loader />;
 
